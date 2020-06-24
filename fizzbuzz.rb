b05996f8b8f8885fc57ca8e3ffc595085e8615d2
Kevin Iturralde <keviniturralde3@gmail.com>
2020-06-24 17:24:46 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0
-    puts "fizz"
-    if int % 5 == 0
-      puts "buzz"
-    else int % 5 == 0 || int % 3 == 0
-      puts "fizzbuzz"
-end
-end
-fizzbuzz (3)
+  if int % 3 == 0 &&  int % 5 == 0
+    "FizzBuzz"
+  elsif int % 5 == 0
+     "Buzz"
+    elsif int % 3 == 0
+    "Fizz"
+    end
+  end
+puts fizzbuzz (3)
+puts fizzbuzz (5)
+puts fizzbuzz (15)
